9382eff6dcc05d515b02222a9d7e61c304d67a69
wilmerG123 <garnicawilmer@gmail.com>
2021-09-06 19:20:32 -0500

    subiendo proyecto

---
+++ src/test/java/com/autenticacion/AutenticacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autenticacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
